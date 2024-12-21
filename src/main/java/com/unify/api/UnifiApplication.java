5ee2e893adab15485f345bc31fd0b94fa8b57f78
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2024-12-20 22:57:48 -0500

    first commit

---
+++ src/main/java/com/unify/api/UnifiApplication.java
@@ -0,0 +1,13 @@
+package com.unify.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnifiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnifiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unify/api/UnifiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unify.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
